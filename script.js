149d56ceab2fe18a9604da755ed7b74f310f34bc
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-05 09:59:19 +0200

    #Milestone #1 Done

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 //Preparazione: Verifica collegamento JS/Recupero l'elemento di interesse del Dom 
 console.log('JS OK')
 const container = document.getElementById('container');
+//Creo un ciclo for che stampi in console i numeri da 1 a 100, con variazioni del nome per multipli di 3, di 5 e di entrambi  ;
+for(let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+
+    } else if(i % 3 === 0) {
+        console.log('Fizz');
+        
+    } else if(i % 5 === 0) {
+        console.log('Buzz');
+    }
+     else {
+        console.log(i) ;
+    }
+}
